7655c56a013cc750316e2ac65f31c286ec8cd899
koreaunnie <hyunjung1995@gmail.com>
2024-11-06 13:04:15 +0900

    export, import

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App2 from "./practice/App2.jsx";
+import MyApp12 from "./apps/MyApp12.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App2 />
+    <MyApp12 />
   </StrictMode>,
 );
