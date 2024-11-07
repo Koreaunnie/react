eea443c0afe3fe19819ceec14633e0b7894ce1eb
koreaunnie <hyunjung1995@gmail.com>
2024-11-07 16:38:18 +0900

    useState 를 활용하여 버튼 눌러 박스 모양 바꾸기

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp39 from "./apps/MyApp39.jsx";
+import App39 from "./practice/App39.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp39 />
+      <App39 />
     </Provider>
   </StrictMode>,
 );
