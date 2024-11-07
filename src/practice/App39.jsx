c8a82caac6aaa8bf6c749815820c6f4b1ffda961
koreaunnie <hyunjung1995@gmail.com>
2024-11-07 16:41:16 +0900

    useState 를 활용하여 버튼 눌러 박스 모양 바꾸기

--- src/practice/App39.jsx
+++ src/practice/App39.jsx
@@ -15,20 +15,43 @@ function App39(props) {
     }
   }
 
+  // css display 만 none이라 화면에는 안보이지만 html 구조상 존재
   return (
     <div>
       <Button onClick={handleClick}>{number}</Button>
-      <Box display={number == 1 ? "block" : "none"} h={20} bg={"blue.300"}>
+      <Box display={number === 1 ? "block" : "none"} h={20} bg={"blue.300"}>
         1
       </Box>
-      <Box display={number == 2 ? "block" : "none"} h={20} bg={"red.300"}>
+      <Box display={number === 2 ? "block" : "none"} h={20} bg={"red.300"}>
         2
       </Box>
-      <Box display={number == 3 ? "block" : "none"} h={20} bg={"yellow.300"}>
+      <Box display={number === 3 ? "block" : "none"} h={20} bg={"yellow.300"}>
         3
       </Box>
     </div>
   );
+
+  // html 구조상 box가 1개만 보이게
+  // return (
+  //   <div>
+  //     <Button onClick={handleButtonClick}>{number}</Button>
+  //     {number === 1 && (
+  //       <Box h={20} bg={"blue.300"}>
+  //         1
+  //       </Box>
+  //     )}
+  //     {number === 2 && (
+  //       <Box h={20} bg={"red.300"}>
+  //         2
+  //       </Box>
+  //     )}
+  //     {number === 3 && (
+  //       <Box h={20} bg={"yellow.300"}>
+  //         3
+  //       </Box>
+  //     )}
+  //   </div>
+  // );
 }
 
 export default App39;
