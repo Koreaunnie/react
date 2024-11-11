5bde50f18b33da1db0dd4add9ce7e8f606b23d57
koreaunnie <hyunjung1995@gmail.com>
2024-11-11 11:23:35 +0900

    useEffect의 cleanUp 함수

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp52 from "./apps/MyApp52.jsx";
+import MyApp54 from "./apps/MyApp54.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp52 />
+      <MyApp54 />
     </Provider>
   </StrictMode>,
 );
