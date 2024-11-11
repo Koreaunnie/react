05f39b92d08fef151ba869d6dc0a3013acbbc2e8
koreaunnie <hyunjung1995@gmail.com>
2024-11-11 10:18:22 +0900

    component lifecycle

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import App51 from "./practice/App51.jsx";
+import MyApp52 from "./apps/MyApp52.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App51 />
+      <MyApp52 />
     </Provider>
   </StrictMode>,
 );
