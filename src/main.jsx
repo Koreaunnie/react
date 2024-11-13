3284a8ed95388fc8ba636e608bcb42ed31f2f10c
koreaunnie <hyunjung1995@gmail.com>
2024-11-13 16:23:59 +0900

    react router dynamic parameter (useParams) 경로를 데이터로

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App76 from "./practice/App76.jsx";
+import MyApp77 from "./apps/MyApp77.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App76 />
+      <MyApp77 />
       <Toaster />
     </Provider>
   </StrictMode>,
