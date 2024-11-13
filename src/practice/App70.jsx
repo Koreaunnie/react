7d5b8a92135b458df42727d217208325d1ec1a14
koreaunnie <hyunjung1995@gmail.com>
2024-11-13 11:47:47 +0900

    여러 파일 전송 : postForm

---
+++ src/practice/App70.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App70(props) {
+  const [name, setName] = useState("");
+  const [files, setFiles] = useState([]);
+
+  const handleClickButton = () => {
+    axios.postForm("/api/main8/sub4", {
+      name: name,
+      files: files,
+    });
+  };
+
+  return (
+    <div>
+      <input
+        type={"text"}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <input type="file" multiple onChange={(e) => setFiles(e.target.files)} />
+      <br />
+      <button onClick={handleClickButton}>전송</button>
+    </div>
+  );
+}
+
+export default App70;
